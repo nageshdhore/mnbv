e5679906507016e75b3365654d056d1bb7f3d9f9
nagesh dhore <nageshdhore1993@gmail.com>
2022-11-16 09:02:28 +0530

    keyword

---
+++ src/q123e/weesa.java
@@ -0,0 +1,13 @@
+package q123e;
+
+public class weesa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+sdjjhjhclhkl
+shklklkljkljkj
+kkckjkl;l;c
+cklkjkjl;
+	}
+
+}
